1a1deb26e8b3276ab7e3f2a461b12d16d7de702a
Brent Dady <brentdady@gmail.com>
2015-05-28 13:41:55 -0700

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Brent Dady on 5/28/15.
+//  Copyright (c) 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
